314e19adbaa0c3d158ea19bf21251cd2384d23b8
brettmadrid <brettmadrid9@gmail.com>
2020-02-12 14:18:30 -0800

    increased functionality

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,45 @@
 import React from "react";
 import "./App.css";
 
-const BottomRow = () => {
+const BottomRow = ({ down, setDown, toGo, setToGo, quarter, setQuarter }) => {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   return (
     <div className="bottomRow">
       <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <button
+          className="down__title"
+          onClick={down !== 4 ? () => setDown(down + 1) : () => setDown(1)}
+        >
+          Down
+        </button>
+        <div className="down__value">{down}</div>
       </div>
       <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <button
+          className="toGo__title"
+          onClick={() => setToGo(getRandomInt(10))}
+        >
+          To Go
+        </button>
+        <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
+        <button className="ballOn__title">Ball on</button>
         <div className="ballOn__value">21</div>
       </div>
       <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <button
+          className="quarter__title"
+          onClick={
+            quarter !== 4 ? () => setQuarter(quarter + 1) : () => setQuarter(1)
+          }
+        >
+          Quarter
+        </button>
+        <div className="quarter__value">{quarter}</div>
       </div>
     </div>
   );
